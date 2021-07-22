ee542bc62d1deaad9921a343245c4bebf729a554
eduardosilva218 <eduardo.silva218@outlook.com>
2021-07-22 02:48:16 -0300

    first commit

---
+++ PrancingPonySharp.Test/DataStructures/Maybe/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace PrancingPonySharp.Test.DataStructures.Maybe
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ PrancingPonySharp/DataStructures/Maybe/Maybe.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PrancingPonySharp.DataStructures.Maybe
+{
+    public readonly struct Maybe<T>
+    {
+        private T Value { get; }
+
+        public bool IsJust { get; }
+
+        public bool IsNothing => !IsJust;
+
+        public static implicit operator Maybe<T>(T value) =>
+            Just(value);
+
+        private Maybe(T value, bool isJust)
+        {
+            Value = value;
+            IsJust = isJust;
+        }
+
+        public static Maybe<TD> Just<TD>(TD value) => 
+            new(value, value is not null);
+
+        public TR Match<TR>(Func<T, TR> just, Func<TR> nothing) => 
+            IsJust ? just(Value) : nothing();
+
+        public void Match(Action<T> just, Action nothing)
+        {
+            if (IsJust)
+                just(Value);
+            else
+                nothing();
+        }
+    }
+}
