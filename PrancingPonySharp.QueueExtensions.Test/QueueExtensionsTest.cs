35e35a10a0fcd5f103a6045ac636701f42ff88d1
Eduardo Silva <eduardo@sistemasbr.net>
2022-01-21 14:46:19 -0300

    Test correction.

--- PrancingPonySharp.QueueExtensions.Test/QueueExtensionsTest.cs
+++ PrancingPonySharp.QueueExtensions.Test/QueueExtensionsTest.cs
@@ -64,7 +64,7 @@ namespace PrancingPonySharp.QueueExtensions.Test
             const int quantity = 200000;
             var exception = Assert.Throws<IndexOutOfRangeException>(
                 () => actual.Dequeue(quantity));
-            Assert.Equal($"The length is {actual.Count} but the quantity is {quantity}.", exception.Message);
+            Assert.Equal($"Attempted to dequeue an invalid amount of values: The queue's length is {actual.Count} but the amount expected to dequeue is {quantity}.", exception.Message);
         }
 
         [Fact]
