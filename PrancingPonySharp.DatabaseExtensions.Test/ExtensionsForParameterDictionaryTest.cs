607d7ca9956b2d3b8c9451331f30f2d73251635c
Eduardo Silva <eduardo@sistemasbr.net>
2022-02-16 18:00:29 -0300

    Method to convert dictionary in ienumerable.

--- PrancingPonySharp.DatabaseExtensions.Test/ExtensionsForParameterDictionaryTest.cs
+++ PrancingPonySharp.DatabaseExtensions.Test/ExtensionsForParameterDictionaryTest.cs
@@ -1,16 +1,17 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using Moq;
 using PrancingPonySharp.DatabaseExtensions.Test.Mock;
 using Xunit;
 
 namespace PrancingPonySharp.DatabaseExtensions.Test
 {
-    public class ExtensionsForIDataParameterCollectionTest
+    public class ExtensionsForParameterDictionaryTest
     {
         [Fact]
-        public void ShouldBeAddParametersInIDbCommand()
+        public void ShouldConvertTheDictionaryIntoIEnumerableOfIDbDataParameter()
         {
             var dbDataParameterMock = new Mock<IDbDataParameter>();
             dbDataParameterMock.SetupProperty(x => x.Value);
@@ -21,39 +22,24 @@ namespace PrancingPonySharp.DatabaseExtensions.Test
                 .Returns(dataParameterCollectionMockObject);
             dbCommandMock.Setup(x =>
                 x.CreateParameter()).Returns(dbDataParameterMock.Object);
-            var expected = new Dictionary<string, object>
-            {
-                {"x", 1},
-                {"y", 2},
-                {"z", 3}
-            };
-            dbCommandMock.Object.AddParameters(expected);
-            Assert.Equal(expected, dataParameterCollectionMockObject.Parameters);
-        }
-        
-        [Fact]
-        public void ShouldBeAddTwoParametersInIDbCommand()
-        {
-            var dbDataParameterMock = new Mock<IDbDataParameter>();
-            dbDataParameterMock.SetupProperty(x => x.Value);
-            dbDataParameterMock.SetupProperty(x => x.ParameterName);
-            var dbCommandMock = new Mock<IDbCommand>();
-            var dataParameterCollectionMockObject = new DataParameterCollectionMockObject();
-            dbCommandMock.SetupGet(x => x.Parameters)
-                .Returns(dataParameterCollectionMockObject);
-            dbCommandMock.Setup(x =>
-                x.CreateParameter()).Returns(dbDataParameterMock.Object);
-            const int expected = 2;
-            dbCommandMock.Object.AddParameters(new Dictionary<string, object>
+            var parameterDictionary = new Dictionary<string, object>
             {
                 {"x", 1},
                 {"y", 2}
+            };
+            var expected = parameterDictionary.Select(parameter =>
+            {
+                var dataParameter = dbCommandMock.Object.CreateParameter();
+                var (key, value) = parameter;
+                dataParameter.ParameterName = key;
+                dataParameter.Value = value;
+                return dataParameter;
             });
-            Assert.Equal(expected, dataParameterCollectionMockObject.Parameters.Count);
+            Assert.Equal(expected, parameterDictionary.ConvertToParameters(dbCommandMock.Object));
         }
         
         [Fact]
-        public void ShouldThrowAnNullReferenceExceptionWithTheCorrectMessageWhenPassingNullAsParameters()
+        public void ShouldThrowAnNullReferenceExceptionWithTheCorrectMessageWhenPassingNullAsIDbCommandToConvertToParameters()
         {
             var dbDataParameterMock = new Mock<IDbDataParameter>();
             dbDataParameterMock.SetupProperty(x => x.Value);
@@ -64,9 +50,14 @@ namespace PrancingPonySharp.DatabaseExtensions.Test
                 .Returns(dataParameterCollectionMockObject);
             dbCommandMock.Setup(x =>
                 x.CreateParameter()).Returns(dbDataParameterMock.Object);
+            var parameterDictionary = new Dictionary<string, object>
+            {
+                {"x", 1},
+                {"y", 2}
+            };
             var exception = Assert.Throws<NullReferenceException>(() => 
-                dbCommandMock.Object.AddParameters(null));
-            Assert.Equal("Attempt to add invalid parameters: It is not possible to add null variables as parameters.",
+                parameterDictionary.ConvertToParameters(null));
+            Assert.Equal("Attempt to use invalid command: It is not possible to use null variables as IDbCommand.",
                 exception.Message);
         }
     }
---
+++ PrancingPonySharp.DatabaseExtensions/ExtensionsForIDbCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace PrancingPonySharp.DatabaseExtensions
+{
+    public static class ExtensionsForIDbCommand
+    {
+        public static void AddParameters(this IDbCommand command, IEnumerable<IDbDataParameter> parameters)
+        {
+            if (parameters is null)
+            {
+                const string exceptionMessage = "Attempt to add invalid parameters: It is not possible to add null variables as parameters.";
+                throw new NullReferenceException(exceptionMessage);
+            };
+            foreach (var parameter in parameters) 
+                command.Parameters.Add(parameter);
+        }
+    }
+}
